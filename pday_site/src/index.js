5a3f9c905f908c83825728b7ba2b87f7a0200aad
Aditya Agrawal <adityaagrawal@Adityas-MacBook-Pro.local>
2023-03-07 23:16:02 +0800

    working raytracer --> needs to be rewritten with react-three-fiber

--- pday_site/src/index.js
+++ pday_site/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import SDFPage from "./pages/SDFPage";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<SDFPage />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
